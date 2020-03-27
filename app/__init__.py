eb339466fd6b539f2e1b7253a8b83a34271c09f8
DocMorg <vlad_ustimov@mail.ru>
2020-03-28 01:44:55 +0300

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -14,11 +14,10 @@ def create_app(config_class=Config):
     ctx = app.app_context()
     ctx.push()
 
-    db.init_app(app)
-    # bot.set_webhook(Config.URL.format(Config.secret))
+    with app.app_context():
+        db.create_all()
 
+    bot.set_webhook(Config.URL.format(Config.secret))
     migrate = Migrate(app, db)
-
-    from app import routes, models
 
     return app
--- app_.py
+++ app_.py
@@ -3,6 +3,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True, threaded=True)
