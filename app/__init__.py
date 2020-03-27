8bba207974beece4028b45882b993f8eb660906b
DocMorg <vlad_ustimov@mail.ru>
2020-03-28 01:31:13 +0300

    create db

--- app/__init__.py
+++ app/__init__.py
@@ -3,20 +3,19 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from app.config import Config
 import telebot
-import time
 
 db = SQLAlchemy()
 bot = telebot.TeleBot(Config.secret, threaded=False)
 bot.remove_webhook()
 
-
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
+    ctx = app.app_context()
+    ctx.push()
 
     db.init_app(app)
-    time.sleep(1)
-    bot.set_webhook(Config.URL.format(Config.secret))
+    # bot.set_webhook(Config.URL.format(Config.secret))
 
     migrate = Migrate(app, db)
 
