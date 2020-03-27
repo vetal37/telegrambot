c5921b41dd3acb81f1a283e1c753d4273b7f42f8
vetal37 <48363991+vetal37@users.noreply.github.com>
2020-03-27 12:16:47 +0400

    Add files via upload

--- bot/suka.py
+++ bot/suka.py
@@ -1,2 +1,11 @@
+import sqlite3
+import telegram
+
+base = sqlite3.connect("database.db")
+cursor = base.cursor()
+
+cursor.execute("""create table tables_with_students (teacher_id integer, teacher_name text, teacher_table_link text, teacher_table_name text)""")
+
 def get_response(msg):
-    return "ъуъ"
+    if msg == "test":
+        return "msg"
