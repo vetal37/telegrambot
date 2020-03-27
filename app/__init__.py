8d820d0bca24c2467feeec19dbbe53719978d414
DocMorg <vlad_ustimov@mail.ru>
2020-03-28 00:32:43 +0300

    update

--- app/__init__.py
+++ app/__init__.py
@@ -13,12 +13,10 @@ bot.remove_webhook()
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
-    ctx = app.app_context()
-    ctx.push()
 
     db.init_app(app)
     time.sleep(1)
-    bot.set_webhook(url="https://vetal37.pythonanywhere.com/{}".format(Config.secret))
+    bot.set_webhook(Config.URL.format(Config.secret))
 
     migrate = Migrate(app, db)
 
