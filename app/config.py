c639363f6c9d9458cabe541f2664bae7b564b0b5
DocMorg <vlad_ustimov@mail.ru>
2020-03-28 15:05:23 +0300

    update

--- app/config.py
+++ app/config.py
@@ -3,6 +3,6 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     bot_token = "960088688:AAGVD9I_T8TO1oW0L3H9NQyKIjyPpYU6ZZE"
     bot_user_name = "test3712356136_bot"
-    URL = "fathomless-headland-73674/"
+    URL = "https://fathomless-headland-73674.herokuapp.com/"
     # service_email = "acc-491@woven-environs-272314.iam.gserviceaccount.com"
     secret = "960088688:AAGVD9I_T8TO1oW0L3H9NQyKIjyPpYU6ZZE"
