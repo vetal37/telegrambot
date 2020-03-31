a4fc0ea9c9ca2271a1c3c3f17784ffc1e8af323d
Vitaliy Matyunin <into_the_night@bk.ru>
2020-03-31 07:05:36 +0400

    updated tables, added todo in routes

--- app/google_tables/tables.py
+++ app/google_tables/tables.py
@@ -5,6 +5,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 from app.models import Teacher, Tables
 import datetime
 import time
+#import app.routes
 
 
 CREDENTIALS_FILE = 'woven-environs-272314-a2f4d17f757a.json'  # Имя файла с закрытым ключом, вы должны подставить свое
@@ -65,7 +66,7 @@ ranges = {
     }}
 
 def fill_in_date_in_table(spreadsheet_id, list_id, list_name):
-    ranges = [list_name + "!A1:AAA1"]
+    ranges_name = [list_name + "!A1:AAA1"]
     results = service.spreadsheets().values().batchGet(spreadsheetId = spreadsheet_id, 
                                                    ranges = ranges_name, 
                                                    valueRenderOption = 'FORMATTED_VALUE',  
@@ -76,19 +77,45 @@ def fill_in_date_in_table(spreadsheet_id, list_id, list_name):
         if sheet_counter < 27:
             cell_code = letter[sheet_counter]
         else:
-            cell_code = letter[sheet_counter // 26] + letter[sheet_counter % 26]
+            cell_code = letter[(sheet_counter // 26) - 1] + letter[sheet_counter % 26]
     cell_name = cell_code + "1"
-    ranges = [list_name + "!" + cell_name]    
-    service.spreadsheets().values().batchUpdate(spreadsheetId = spreadsheetId, body = {
+    ranges_name = [list_name + "!" + cell_name]    
+    service.spreadsheets().values().batchUpdate(spreadsheetId = spreadsheet_id, body = {
                     "valueInputOption": "USER_ENTERED", # Данные воспринимаются, как вводимые пользователем (считается значение формул)
                     "data": [
-                            {"range": ranges,
+                            {"range": ranges_name,
                             "majorDimension": "ROWS",     # Сначала заполнять строки, затем столбцы
                             "values": [time.strftime("%d.%m.%Y", datetime.date.today())]}
     ]
     }).execute()
     return cell_code
 
-def vote_for_best_student(ranges, sheet_counter):
-    
+def vote_for_best_student(ranges, sheet_counter, student_id):
+    student_on_lesson_column = fill_in_date_in_table(spreadsheet_id, list_id, list_name)
+    ranges_name = [list_name + "!" + student_on_lesson_column + "1:" + student_on_lesson_column + str(sheet_counter)]
+    results = service.spreadsheets().values().batchGet(spreadsheetId = spreadsheet_id, 
+                                                   ranges = ranges_name, 
+                                                   valueRenderOption = 'FORMATTED_VALUE',  
+                                                   dateTimeRenderOption = 'FORMATTED_STRING').execute()
+    if int(rating) != 0: #TODO function for voting in routes.py
+#Идея такая: каждому челу на паре (отмеченному в таблице) присваивается student_id = номеру его строки
+#тогда если проголосовал: new_results = results[student_id] + 1
+#и это пишется в таблицу /done
+        new_results = int(results[student_id]) + 1
+        ranges_name_student = [list_name + "!" + student_on_lesson_column + str(student_id)]
+        service.spreadsheets().values().batchUpdate(spreadsheetId = spreadsheet_id, body = {
+                    "valueInputOption": "USER_ENTERED", # Данные воспринимаются, как вводимые пользователем (считается значение формул)
+                    "data": [
+                            {"range": ranges_name_student,
+                            "majorDimension": "ROWS",     # Сначала заполнять строки, затем столбцы
+                            "values": [new_results]}
+                            ]
+                            }).execute()
+#TODO почекай, что тут ещё надо для голосовалки
+    return #TODO variable to return
 
+def fill_in_names_and_phones():
+    #TODO а как с базой работать? Скинь документацию
+    #TODO заполнение таблицы из базы данных
+    #TODO сегодня сделаю, если кинешь
+    return #TODO variable to return
