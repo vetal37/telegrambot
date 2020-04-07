8fb0c66a07635a0202a26c01056f7f23ab4c6955
DocMorg <vlad_ustimov@mail.ru>
2020-04-07 03:56:31 +0300

    update

--- app/routes.py
+++ app/routes.py
@@ -174,8 +174,7 @@ def teacher_table_delete_step1(message):
     try:
         chat_id = message.chat.id
         keyboard = types.InlineKeyboardMarkup()
-        for i in Tables.query.filter_by(
-                Tables.user_id == str(chat_id)).all().list_name:
+        for i in Tables.query.filter(user_id=str(chat_id)).all().list_name:
             keyboard.add(types.InlineKeyboardButton(text=i, callback_data="delete2"))
         bot.send_message(chat_id, text='Выберите таблицу для удаления', reply_markup=keyboard)
     except Exception as e:
