61bb79448ac351a555a2f8d2984c226ded67cc65
DocMorg <vlad_ustimov@mail.ru>
2020-04-07 03:37:49 +0300

    update

--- app/__init__.py
+++ app/__init__.py
@@ -5,15 +5,17 @@ from app.config import Config
 import telebot
 import time
 
-db = SQLAlchemy()
 bot = telebot.TeleBot(Config.secret, threaded=True)
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    db = SQLAlchemy()
+    db.app = app
     db.init_app(app)
 
+    app.app_context().push()
     with app.app_context():
         from app import routes
         db.create_all()
